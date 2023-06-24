2c2893a8dcf64a1662c4413a22d3c3eb8489a11b
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-24 20:02:04 +0300

    create meals componnets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>{" "}
     </Fragment>
   );
 }
