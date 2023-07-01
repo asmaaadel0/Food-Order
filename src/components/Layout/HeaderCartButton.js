e1efcc175bcebfdcf34c5eec642ff027aa717804
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-07-01 14:46:26 +0300

    update number of cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartctx = useContext(CartContext);
+
+  const numberOfCartItems = cartctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>{" "}
-      <span> Your Cart </span> <span className={classes.badge}> 3 </span>{" "}
+      <span> Your Cart </span>{" "}
+      <span className={classes.badge}> {numberOfCartItems} </span>{" "}
     </button>
   );
 };
